cd664aebd63c8662dbc1a4b4b8924bb81aa7d40b
DOlDNa <info@garihari.sakura.ne.jp>
2023-02-13 14:19:18 +0900

    Update Kotmix.php

--- Kotmix.php
+++ Kotmix.php
@@ -17,22 +17,11 @@ function b($b)
 	}
 	unset($e, $d);
 }
-?>
-<!doctype html>
-<html lang=ja>
-	<head>
-		<meta charset=utf-8>
-		<meta name=viewport content="width=device-width,initial-scale=1">
-		<title>Kotmix</title>
-		<style>body{background:#222;color:#ccc;margin:0}img{cursor:pointer;display:block;margin:0 auto;max-width:100%}</style>
-	</head>
-	<body><?php ob_flush();
-		if (($a = glob('/var/tmp/*.tar.gz')) && isset($a[0]))
-		{
-			$b = iterator_to_array(new RecursiveIteratorIterator(new RecursiveDirectoryIterator('phar://'. $a[0]), RecursiveIteratorIterator::CHILD_FIRST));
-			natsort($b);
-			foreach ($b as $c) a(b($c));
-		}
-		unset($a, $b, $c)?><script>let i,w,a=document.images;for(i in a){a[i].onclick=function(){w=window.open(),d=w.document,s=d.createElement('script');s.innerText='let b=document.body,c;b.addEventListener("mousedown",function(e){b.style.cursor="grabbing";c=1;x=e.clientX;y=e.clientY;e.preventDefault()});b.addEventListener("mouseup",function(){b.style.cursor="grab";c=0});b.addEventListener("mousemove",function(e){if(c){b.style.cursor="grabbing";b.scrollLeft-=(-x+(x=e.clientX));b.scrollTop-=(-y+(y=e.clientY))}})';d.write(this.outerHTML);d.title=this.alt+' - '+document.title;d.body.style.backgroundColor='#222';d.body.style.margin=0;d.body.appendChild(s)}}</script>
-	</body>
-</html>
+?><!doctype html><html lang=ja><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1"><title>Kotmix</title><style>::-webkit-scrollbar{width:.5em}::-webkit-scrollbar-thumb{background:hotpink}body{background:#222;margin:0}img{cursor:pointer;display:block;margin:0 auto;max-width:100%}</style></head><body><?php ob_flush();
+if (($a = glob('/var/tmp/*.tar.gz')) && isset($a[0]))
+{
+	$b = iterator_to_array(new RecursiveIteratorIterator(new RecursiveDirectoryIterator('phar://'. $a[0]), RecursiveIteratorIterator::CHILD_FIRST));
+	natsort($b);
+	foreach ($b as $c) a(b($c));
+}
+unset($a, $b, $c)?><script>[].slice.call(document.images).forEach(a=>a.onclick=i=>{w=window.open("javascript:opener.globalHTML"),d=w.document,s=d.createElement("script"),t=d.createElement("style"),s.innerText='let b=document.body,c;b.addEventListener("mousedown",e=>{b.style.cursor="grabbing",c=1,x=e.clientX,y=e.clientY,e.preventDefault()}),b.addEventListener("mouseup",()=>{b.style.cursor="grab",c=0}),b.addEventListener("mousemove",e=>{if(c)b.style.cursor="grabbing",b.scrollLeft-=(-x+(x=e.clientX)),b.scrollTop-=(-y+(y=e.clientY))})',t.innerText='::-webkit-scrollbar{height:.5em;width:.5em}::-webkit-scrollbar-thumb,::-webkit-scrollbar-corner{background:hotpink}body{background:#222;margin:0}',d.write(i.target.outerHTML),d.head.appendChild(t),d.head.appendChild(s),d.title=i.target.alt+" - "+document.title})</script></body></html>
